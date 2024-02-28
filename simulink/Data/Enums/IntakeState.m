759e2ac2a0a611f1ac4fd72a944206b9a5a13cf5
Janelyn Anderson <andersonjanelyn@gmail.com>
2024-02-27 20:00:45 -0600

    More

--- simulink/Data/Enums/IntakeState.m
+++ simulink/Data/Enums/IntakeState.m
@@ -1,23 +1,7 @@
-classdef untitled
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
-    
-    properties
-        Property1
-    end
-    
-    methods
-        function obj = untitled(inputArg1,inputArg2)
-            %UNTITLED Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
-        end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
+classdef IntakeState < Simulink.IntEnumType
+    enumeration
+        On(1)
+        Off(0)
     end
 end
 
