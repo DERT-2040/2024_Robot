b2093f4ea5553f264d2349688a8c4002fe072974
Janelyn Anderson <andersonjanelyn@gmail.com>
2024-03-04 19:05:18 -0600

    WOW

--- src/main/include/Intake.h
+++ src/main/include/Intake.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <TimeOfFlight.h>
-#include <ctre/phoenix/motorcontrol/can/TalonSRX.h>
+#include <rev/CANSparkMax.h>
 #include "include/Constants.h"
 #include "Code_Gen_Model_ert_rtw/Code_Gen_Model.h"
 
@@ -11,5 +11,5 @@ public:
     void PostStep();
 private:
     frc::TimeOfFlight TOF_Sensor{Constants::Intake_Constants::TOF_ID};
-    ctre::phoenix::motorcontrol::can::TalonSRX Intake_Motor{Constants::Intake_Constants::Intake_Motor_Device_ID};
+    rev::CANSparkMax Intake_Motor{Constants::Intake_Constants::Intake_Motor_Device_ID, rev::CANSparkMax::MotorType::kBrushed};
 };
